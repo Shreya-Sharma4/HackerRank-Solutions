6344a7790e96f8af0d6b62f6a205bc3cd66645dc
Shreya-Sharma4 <sharmashreya13794@gmail.com>
2026-07-02 22:51:16 +0530

    Add solutions for Find a String and String Validators

---
+++ python/19_string_validators.py
@@ -0,0 +1,26 @@
+if __name__ == '__main__':
+    s = input()
+
+    has_alnum = False
+    has_alpha = False
+    has_digit = False
+    has_lower = False
+    has_upper = False
+
+    for ch in s:
+        if ch.isalnum():
+            has_alnum = True
+        if ch.isalpha():
+            has_alpha = True
+        if ch.isdigit():
+            has_digit = True
+        if ch.islower():
+            has_lower = True
+        if ch.isupper():
+            has_upper = True
+
+    print(has_alnum)
+    print(has_alpha)
+    print(has_digit)
+    print(has_lower)
+    print(has_upper)
