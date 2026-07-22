e9d4ebdfc9482b06e9a15efdc4f4e68230a8fcbe
Shreya-Sharma4 <sharmashreya13794@gmail.com>
2026-07-22 09:19:20 +0530

    Add solution for Set .discard(), .remove() & .pop() challenge

--- python/45_set_discard_remove_pop.py
+++ python/45_set_discard_remove_pop.py
@@ -1,15 +1,21 @@
 n = int(input())
-s = set(map(int, input().split()))
-command=int(input())
-for i in range(command):
-    choice=input().split()
-    if len(choice)==2:
-        element = int(choice[-1])
-        if choice[0]=="remove":
-            s.remove(element)
-        elif choice[0]=="discard":
-            s.discard(element)
-    elif choice[0]=="pop":
-        s.pop()
-        
-print(sum(s))
+numbers = set(map(int, input().split()))
+
+number_of_commands = int(input())
+
+for _ in range(number_of_commands):
+    command = input().split()
+
+    if len(command) == 2:
+        operation = command[0]
+        element = int(command[1])
+
+        if operation == "remove":
+            numbers.remove(element)
+        elif operation == "discard":
+            numbers.discard(element)
+
+    elif command[0] == "pop":
+        numbers.pop()
+
+print(sum(numbers))
